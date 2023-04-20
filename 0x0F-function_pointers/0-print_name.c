400ae080dfea06842c2ea06535f2fee9098ae349
neomtshali <s.neomtshali@gmail.com>
2023-04-19 18:49:24 -0700

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,8 @@
 /**
  * print_name - print name using pointer to function
  * @name: string to add
- *@f: pointer to function
- *Return: nothing
+ * @f: pointer to function
+ * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
 {
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  * @size: how many elem to print
  * @action: pointer to print in regular or hex
  * Return: void
- */
+**/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
